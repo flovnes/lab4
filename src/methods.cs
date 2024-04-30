2f36320246d4f9029934c273594ed75df859de05
Dmytro Kyba <dkyba2006@gmail.com>
2024-04-30 16:14:17 +0300

    changed the access modificators and wrote the tests for my case

--- src/methods.cs
+++ src/methods.cs
@@ -2,67 +2,76 @@ namespace struct_lab_student
 {
 	public partial class Program
 	{
-		static void Var9(List<Student> students) { }
-
-		static void Var10(List<Student> students)
+		public static void Var9(List<Student> students)
 		{
-			static double CountAverageScore(Student student)
+			WriteData(ProcessData(students), "../data_new.txt");
+			Console.WriteLine("\nДані збережено у data_new.txt");
+		}
+
+		public static void Var10(List<Student> students)
+		{
+			Console.WriteLine("\nСписок студентів з відмінними оцінками з фізики:");
+			foreach (var student in Filter(students, IsTalentedPhysician))
+				Console.WriteLine($"{student.surName} {student.firstName} {student.patronymic} {EvalAverage(student):0.0} {student.scholarship}");
+		}
+
+		public static void Var24(List<Student> students)
+		{
+			Console.WriteLine("\nСписок студентів, які народились влітку:");
+			var summer_kids = Filter(students, IsSummer);
+			foreach (var dude in summer_kids)
+				Console.WriteLine($"\r{dude.surName} {dude.firstName} {EvalAverage(dude):0.0}");
+		}
+
+		private static List<Student> ProcessData(List<Student> students)
+		{
+			List<Student> processedStudents = [];
+
+			foreach (var student in students)
 			{
-				char[] marks = student.Marks;
-				double sum = 0;
-				foreach (var c in marks)
+				var newStudent = student;
+				processedStudents.Add(newStudent);
+				newStudent.sex = (student.sex == 'M' || student.sex == 'Ч') ? 'Ч' : (student.sex == 'F' || student.sex == 'Ж') ? 'Ж' : '?';
+			}
+
+			return processedStudents;
+		}
+
+		private static void WriteData(List<Student> students, string filename)
+		{
+			var max_len = students.Max(s => s.surName.Length);
+			using (StreamWriter writer = new(filename))
+			{
+				foreach (var student in students)
 				{
-					sum += (c == '-') ? 2 : double.Parse(c.ToString());
+					writer.WriteLine($"{student.surName.PadRight(max_len + 2)} {student.firstName.PadRight(max_len + 2)} {student.patronymic.PadRight(max_len + 2)} " +
+									$"{student.sex,-3} {student.dateOfBirth,-12} {student.Marks[0],-3} {student.Marks[1],-3} {student.Marks[2],-3} " +
+									$"{student.scholarship}");
 				}
-
-				return Math.Round(sum / 3, 1);
-			}
-
-			static IEnumerable<Student> FilterTalentedPhysicians(List<Student> students)
-			{
-				return students.Where(student => student.Marks[1] == '5');
-			}
-
-			foreach (var student in FilterTalentedPhysicians(students))
-			{
-				Console.WriteLine($"{student.surName} {student.firstName} {student.patronymic} {CountAverageScore(student):0.0} {student.scholarship}");
 			}
 		}
 
-		static void Var24(List<Student> students)
+		public static IEnumerable<Student> Filter(List<Student> students, Func<Student, bool> Condition)
 		{
-			var summer_kids = FilterSummerKids(students);
-			foreach (var (dude, avg) in summer_kids.Zip(EvalAverages(summer_kids)))
-				Console.WriteLine($"{dude.surName} {dude.firstName} {avg:0.0}");
+			return students.Where(Condition);
 		}
 
-		public static IEnumerable<Student> FilterSummerKids(List<Student> students)
+		public static bool IsSummer(Student student)
 		{
-			return students.Where(student =>
-			{
-				if (DateTime.TryParseExact(student.dateOfBirth, "dd.MM.yyyy", null, System.Globalization.DateTimeStyles.None,
-					out DateTime date)
-					)
-				{
-					return date.Month >= 6 && date.Month <= 8;
-				}
-				return false;
-			});
+			int month = int.Parse(student.dateOfBirth[3..5]);
+			return month >= 6 && month <= 8;
 		}
 
-		public static List<double> EvalAverages(IEnumerable<Student>? students)
+		public static bool IsTalentedPhysician(Student student)
 		{
-			List<double> averages = [];
-			if (students != null)
-			{
-				foreach (var dude in students)
-				{
-					int sum = 0;
-					for (int i = 0; i < 3; i++) sum += (dude.Marks[i] == '-') ? 2 : dude.Marks[i] - '0';
-					averages.Add(sum / 3.0);
-				}
-			}
-			return averages;
+			return student.Marks[1] == '5';
+		}
+
+		public static double EvalAverage(Student student)
+		{
+			double sum = 0;
+			foreach (var c in student.Marks) sum += (c == '-') ? 2 : double.Parse(c.ToString());
+			return Math.Round(sum / 3, 1);
 		}
 	}
 }
